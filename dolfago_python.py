f9fde5f6f39d7116a930d44c7233aeaed47c6b09
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-05-04 00:08:49 +0900

    ability stone game + tistory dolfago

---
+++ dolfago_python.py
@@ -0,0 +1,166 @@
+import gym, gym_lostark
+import numpy as np
+import tqdm
+import random
+
+TRY_N = 100000
+TARGET = (7,6,4)
+
+prob_list = [0.25,0.35,0.45,0.55,0.65,0.75]
+
+def prob_to_idx(prob):
+    return int((prob-0.25)/0.1)
+
+EsMap = np.zeros((100, len(prob_list)))
+EsMap_filled = np.zeros_like(EsMap, dtype=np.bool)
+EaMap = np.zeros((100, 100, len(prob_list)))
+EaMap_filled = np.zeros_like(EaMap, dtype=np.bool)
+Ea1Map = np.zeros((100, 100, 100, len(prob_list)))
+Ea1Map_filled = np.zeros_like(Ea1Map, dtype=np.bool)
+
+
+def succ_prob(p_idx):
+    return min(5, p_idx+1)
+
+def fail_prob(p_idx):
+    return max(0, p_idx-1)
+
+def calculate_E(p_idx, succ, fail, same):
+    prob=prob_list[p_idx]
+    v = prob*succ + (1-prob)*fail
+    if same:
+        v += prob
+    return v
+
+def calculate_Es(n, p_idx):
+    return calculate_E(
+        p_idx,
+        getEs(n, succ_prob(p_idx)),
+        getEs(n, fail_prob(p_idx)),
+        True
+    )
+
+def calculate_Ea(na, nb, p_idx, same):
+    return calculate_E(
+        p_idx,
+        getEa(na, nb, succ_prob(p_idx)),
+        getEa(na, nb, fail_prob(p_idx)),
+        same
+    )
+
+def calculate_Ea1(na1, na2, nb, p_idx, same):
+    return calculate_E(
+        p_idx,
+        getEa1(na1, na2, nb, succ_prob(p_idx)),
+        getEa1(na1, na2, nb, fail_prob(p_idx)),
+        same
+    )
+
+def getEs(n, p_idx):
+    if EsMap_filled[n,p_idx]:
+        pass
+    elif n==0:
+        EsMap[n,p_idx] = 0
+    else:
+        EsMap[n,p_idx] = calculate_Es(n-1, p_idx)
+    EsMap_filled[n,p_idx] = True
+
+    return EsMap[n,p_idx]
+
+def getEa(na, nb, p_idx):
+    if EaMap_filled[na,nb,p_idx]:
+        pass
+    elif na==0:
+        EaMap[na,nb,p_idx] = 0
+    elif nb==0:
+        EaMap[na,nb,p_idx] = getEs(na, p_idx)
+    else:
+        EaA = calculate_Ea(na-1, nb, p_idx, True)
+        EaB = calculate_Ea(na, nb-1, p_idx, False)
+        EaMap[na,nb,p_idx] = max(EaA, EaB)
+    EaMap_filled[na,nb,p_idx] = True
+
+    return EaMap[na,nb,p_idx]
+
+def getEa1(na1, na2, nb, p_idx):
+    if Ea1Map_filled[na1,na2,nb,p_idx]:
+        pass
+    elif na1==0:
+        Ea1Map[na1,na2,nb,p_idx] = 0
+    elif (na2==0) and (nb==0):
+        Ea1Map[na1,na2,nb,p_idx] = getEs(na1, p_idx)
+    elif na2==0:
+        Ea1Map[na1,na2,nb,p_idx] = getEa(na1, nb, p_idx)
+    elif nb==0:
+        Ea1Map[na1,na2,nb,p_idx] = getEa(na1, na2, p_idx)
+    else:
+        Ea1A1 = calculate_Ea1(na1-1, na2, nb, p_idx, True)
+        Ea1A2 = calculate_Ea1(na1, na2-1, nb, p_idx, False)
+        Ea1Map[na1,na2,nb,p_idx] = max(Ea1A1, Ea1A2)
+    Ea1Map_filled[na1,na2,nb,p_idx] = True
+    
+    return Ea1Map[na1,na2,nb,p_idx]
+
+def get_p_table(na1, na2, nb, p_idx):
+    na = na1+na2
+    n = na + nb
+    Es = 0
+    EaA = 0
+    EbA = 0
+    EaB = 0
+    EbB = 0
+    Ea1A1 = 0
+    Ea1A2 = 0
+    Ea2A1 = 0
+    Ea2A2 = 0
+    Es = getEs(n, p_idx)
+    if na>0:
+        EaA = calculate_Ea(na-1, nb, p_idx, True)
+        EbA = Es - EaA
+    if nb>0:
+        EaB = calculate_Ea(na, nb-1, p_idx, False)
+        EbB = Es - EaB
+    if na1>0:
+        Ea1A1 = calculate_Ea1(na1-1, na2, nb, p_idx, True)
+        Ea2A1 = EaA - Ea1A1
+    if na2>0:
+        Ea1A2 = calculate_Ea1(na1, na2-1, nb, p_idx, False)
+        Ea2A2 = EaA - Ea1A2
+    return [
+        [Ea1A1, Ea2A1, EbA],
+        [Ea1A2, Ea2A2, EbA],
+        [EaB, EaB, EbB],
+        max(EaA, EaB)
+    ]
+env = gym.make('AbilityStone-v0')
+success = 0
+for _ in tqdm.trange(TRY_N):
+    o = env.reset(TARGET)
+    done = False
+    while not done:
+        na1, na2, nb, _,_,_, prob = o
+        na1 = int(na1)
+        na2 = int(na2)
+        nb = int(nb)
+        p_idx = prob_to_idx(prob)
+        p_table = get_p_table(na1, na2, nb, p_idx)
+        if (p_table[0][2]==0 and
+            p_table[2][0]==0 and
+            p_table[2][2]>0):
+            action = 2
+        elif p_table[0][2] > p_table[2][2]:
+            action = 2
+        elif (p_table[0][0] > p_table[1][0] and
+              p_table[0][1] < p_table[1][1]):
+            action = 0
+        elif (p_table[0][0] < p_table[1][0] and
+              p_table[0][1] > p_table[1][1]):
+            action = 1
+        else:
+            action = random.randint(0,1)
+        o, r, done, i = env.step(action)
+        if r>0:
+            success += 1
+            break
+print(success)
+print(success/TRY_N)
--- gym_lostark/gym_lostark/envs/ability_stone.py
+++ gym_lostark/gym_lostark/envs/ability_stone.py
@@ -1,4 +1,183 @@
 import gym
+from gym import spaces
+import numpy as np
+
+MAX_PROB = 0.75
+MIN_PROB = 0.25
+# If primary choice is not available
+SECONDARY = [1, 0, 0]
+TERTIARY = [2, 2, 1]
 
 class AbilityStone(gym.Env):
-    pass
+    """AbilityStone
+    LostArk Ability stone simulator
+
+    Observation Space
+    [
+        buff_0_left_chance,
+        buff_1_left_chance,
+        debuff_left_chance,
+
+        buff_0_target_left,
+        buff_1_target_left,
+        debuff_target_left,
+
+        success_probability
+    ]
+
+    Action space
+        0 : buff_0
+        1 : buff_1
+        2 : debuff
+
+        If buff_0 is all filled, 0 becomes 1
+        If buff_1 is all filled, 1 becomes 0
+        If debuff is all filled, 2 becomes 0
+
+        If only one is left, any actions wil choose the left one
+
+    If target is met (buff_0, buff_1 have reached target success
+                      and debuff has failed enough), reward 1 is given.
+    From then, if the stone gets any better (buffs success or debuff fails)
+        reward increases by 1.
+    
+    For example, if the target is 7/6/2 and all debuff chances are used,
+    7/5/2 -> 7/6/2 : Reward 1
+    7/6/2 -> 7/6/2 : Reward 1
+
+    9/5/2 -> 9/5/2 : Reward 0
+    9/5/2 -> 9/6/2 : Reward 3
+
+    If target cannot be reached anyhow, the game ends with reward -1
+
+    Game ends when no chances are left
+    """
+    def __init__(self, **kwargs):
+        """
+        kwargs
+        ------
+        chances : int
+            Initial number of chances available to try (ex. Legend = 9)
+            Defaults to 10
+        """
+        kwargs.setdefault('chances', 10)
+        self._default_chance = np.int32(kwargs['chances'])
+        self._target_result = np.array([10,10,0])
+
+        self._stone = np.ones(3)*self._default_chance
+        self._successes = np.zeros(3)
+        self._fails = np.zeros(3)
+
+        self._rng = np.random.default_rng()
+
+        self._prob = MAX_PROB
+
+        self.action_space = spaces.Discrete(3)
+
+        self.observation_space = spaces.Box(
+            low=np.array([0.0]*6 + [MIN_PROB]),
+            high=np.array([self._default_chance]*6+[MAX_PROB]),
+            dtype=np.float32
+        )
+
+        self._done = False
+        self._initialized = False
+
+
+    def step(self, action):
+        assert self._initialized, 'Reset first before starting env'
+        assert self.action_space.contains(action), 'Invalid action!'
+        assert np.all(self._stone>=0), 'Negative available chances, check code'
+
+        reward = 0
+
+        if self._done:
+            print ('The game is already done. ',
+                    'Continuing may cause unexpected behaviors')
+            return self._get_observation(), reward, self._done, None
+
+        
+        if self._stone[action] == 0:
+            if self._stone[SECONDARY[action]] > 0:
+                self._carve_stone(SECONDARY[action])
+            else:
+                if self._stone[TERTIARY[action]]>0:
+                    self._carve_stone(TERTIARY[action])
+                else:
+                    raise ValueError('No available chances')
+        else:
+            self._carve_stone(action)
+
+        if (self._target_result[0]<=self._successes[0] and
+            self._target_result[1]<=self._successes[1] and
+            self._target_result[2]>=self._stone[2]+self._successes[2]):
+            reward += 1
+            reward += self._successes[0]-self._target_result[0]
+            reward += self._successes[1]-self._target_result[1]
+            reward += self._stone[2]+self._successes[2]-self._target_result[2]
+
+        elif (self._target_result[0]>self._stone[0]+self._successes[0] or
+              self._target_result[1]>self._stone[1]+self._successes[1] or
+              self._target_result[2]<self._successes[2]):
+              self._done = True
+              reward = -1
+        
+        if np.all(self._stone == 0):
+            self._done = True
+
+        return self._get_observation(), reward, self._done, self._successes
+        
+    def _carve_stone(self, action):
+        if self._rng.random()<self._prob:
+            self._stone[action] -= 1
+            self._successes[action] += 1
+            self._prob_success()
+        else:
+            self._stone[action] -= 1
+            self._fails[action] += 1
+            self._prob_fail()
+
+    def _prob_fail(self):
+        self._prob = min(MAX_PROB, self._prob+0.1)
+
+    def _prob_success(self):
+        self._prob = max(MIN_PROB, self._prob-0.1)
+
+    def _get_observation(self):
+        target_left = np.maximum(
+            self._target_result - self._successes,0
+        )
+        return np.float32(np.concatenate(
+            (self._stone, target_left, [self._prob])
+        ))
+
+    
+    def reset(self, target_result):
+        """reset
+        
+        Parameters
+        ----------
+        target_result : [buff_0, buff_1, debuff]
+            buff_0 & buff_1 : Minimum successes (Success is good)
+            debuff : Maximum successes (Success is bad)
+        """
+        self._initialized = True
+        self._prob = MAX_PROB
+        self._stone = np.ones(3)*self._default_chance
+        self._target_result = np.array(target_result)
+        self._successes = np.zeros(3)
+        self._fails = np.zeros(3)
+
+        self._done = False
+
+        return self._get_observation()
+
+
+    def render(self):
+        pass
+
+    def seed(self, seed=None):
+        self._rng = np.random.default_rng(seed=seed)
+
+    def close(self):
+        pass
