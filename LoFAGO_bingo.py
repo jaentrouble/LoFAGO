53cb47b014ff5dcbb35ac1955e4adf0e2bb9fe43
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-05-13 22:32:04 +0900

    bingo drawing

--- LoFAGO_bingo.py
+++ LoFAGO_bingo.py
@@ -1,3 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+import numpy as np
 
+class Console():
+    def __init__(self) -> None:
+        self.root = tk.Tk()
+        self.root.title('KukuBingo')
+        
