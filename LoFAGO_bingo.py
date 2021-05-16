wrong index

LoFAGO_bingo.py
@@ -200,7 +200,7 @@ class Console():
                     else:
                         next_step_idx = (self.step+2)%3
                     next_idx = np.append(next_board.reshape(-1).astype(int),
-                                (self.step+2)%3)
+                                next_step_idx)
                     next_idx = tuple(next_idx)
                     result = self.table[next_idx]
                     next_x = result[0]