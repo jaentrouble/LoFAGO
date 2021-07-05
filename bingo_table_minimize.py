9ed732448dfe347249816949014169086c8c9315
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-07-05 17:40:25 +0900

    merge into one big array

--- bingo_table_minimize.py
+++ bingo_table_minimize.py
@@ -1,12 +1,29 @@
 import numpy as np
 loaded = np.load('bingo_tables/bingo_table_super.npz')
-minimized = {}
+filleds=[]
+tables = []
 for k, v in loaded.items():
     print(k)
-    if len(v.shape)==26: #filled
-        minimized[k] = v
+    if k=='inanna_no_filled':
+        print('no filled')
+        tmp_filled = np.zeros([2]*25+[6],dtype=np.bool)
+        tmp_filled[...,:3] = v
+        filleds.append(tmp_filled)
+    elif k=='inanna_no_table':
+        print('no table')
+        tmp_table = np.zeros([2]*25+[6,3],dtype=np.uint8)
+        tmp_table[...,:3,:] = v[...,:3].astype(np.uint8)
+        tables.append(tmp_table)
+    elif len(v.shape)==26: #filled
+        filleds.append(v)
     elif len(v.shape)==27:
-        minimized[k] = v[...,:3].astype(np.uint8)
+        tables.append(v[...,:3].astype(np.uint8))
     else:
         raise ValueError
-np.savez_compressed('bingo_tables/bingo_table_minimized.npz',**minimized)
+minimized_filled = np.stack(filleds,axis=0)
+minimized_table = np.stack(tables,axis=0)
+np.savez_compressed(
+    'bingo_tables/bingo_table_minimized.npz',
+    table=minimized_table,
+    filled=minimized_filled
+)
