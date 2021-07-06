09c9be04b4338775649f944a8272e4e1874d6101
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2021-07-06 13:29:03 +0900

    faster refresh

--- bingo_table_maker_nth_inanna_neverbingo.py
+++ bingo_table_maker_nth_inanna_neverbingo.py
@@ -149,7 +149,7 @@ def fill_table(initial_tables, nth_inanna, tid, use_tqdm=False):
         
         loop_id = 0
         while len(state_stack)>0:
-            time.sleep(1)
+            time.sleep(0.01)
             last_loop = loop_id
             current_table, step, loop_id = state_stack[-1]
             inanna_step = False
